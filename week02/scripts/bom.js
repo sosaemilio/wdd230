ec194b2005670d48ccb8bbf7db25460adae69571
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-13 00:49:28 -0400

    Activity Learning JavaScript: DOM Manipulation

---
+++ week02/scripts/bom.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+    if (input.value != "") {
+        const listItem = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        listItem.textContent = input.value;
+        deleteButton.textContent = "❌";
+        listItem.append(deleteButton);
+        list.append(listItem);
+
+        deleteButton.addEventListener("click", () => {
+            list.remove(listItem);
+            input.focus();
+        });
+
+        input.focus();
+        input.value = "";
+   };
+});
