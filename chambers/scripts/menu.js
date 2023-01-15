023bc6a6bf8485441f698c8a0d52c2edd35bdc45
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-15 14:34:12 -0400

    Create nav menu and fix site plan link

--- chambers/scripts/menu.js
+++ chambers/scripts/menu.js
@@ -1,3 +1,27 @@
-function myFunction(x) {
+function menuToggle(x) {
   x.classList.toggle("change");
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav(x) {
+  document.querySelector(".nav-menu").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+  menuToggle(x)
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav(x) {
+  document.querySelector(".nav-menu").style.width = "0";
+  menuToggle(x);
+
+}
+
+function menu(x) {
+  menuName = document.querySelector("#menu");
+
+  if (menuName.className === 'menu' ) {
+    openNav(x);
+  } else if (menuName.className === 'menu change')  {
+    closeNav(x);
+  }
 }
