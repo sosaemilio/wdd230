741c3ffd88deec8b657af4c638fefaa34943ea5a
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-29 02:38:56 -0400

    Complete the assigment

--- chambers/scripts/date.js
+++ chambers/scripts/date.js
@@ -1,1 +1,18 @@
-document.getElementById('date').value = new Date();
+//Obtains the submitted time
+const today = new Date(); 
+
+function datetimeLocal(datetime) {
+    // Creates an object to hold the submited date
+    let date = new Date(datetime);
+
+    // Set up the minuts by reducing the UTC Minutes - timezone minezones
+    date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+
+    // Thank
+    return date.toISOString().slice(0, 16);
+}
+
+
+// Append the date into the form
+const date = document.getElementById('date');
+date.value = datetimeLocal(today);
