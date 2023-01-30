d6b6439460b9783c139315ed71a1973cdf396925
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-29 20:43:46 -0400

    fix dark mode button

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -5,7 +5,7 @@ const navBar = document.querySelector("nav");
 const footer = document.querySelector("footer");
 const menu = document.querySelector("#menu-icon");
 const templeLabel = document.querySelector("#temple-label");
-const submitBtn = document.querySelector("#btnSubmit");
+const button = document.querySelector("#btnSubmit");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.src.includes("dark-mode-icon.svg")) {
@@ -18,9 +18,9 @@ modeButton.addEventListener("click", () => {
 		footer.style.background = "#121212";
 		templeLabel.style.background = "#121212";
 		templeLabel.style.color = "#fff";
-		submitBtn.style.background = "#323232";
-		submitBtn.style.color  = "#fff";
-		submitBtn.style.border = "#323232";
+		button.style.background = "#323232";
+		button.style.color  = "#fff";
+		button.style.border = "#323232";
 
 		/* -- Menu Icon -- */
 		modeButton.src = "images/light-mode-icon.svg";
@@ -31,9 +31,9 @@ modeButton.addEventListener("click", () => {
 		main.style.color = "#323232";
 		navBar.style.background = "#DDD0C8";
 		footer.style.background = "#DDD0C8";
-		submitBtn.style.background = "#DDD0C8";
-		submitBtn.style.color = "#323232";
-		submitBtn.style.border = "solid var(--secondary-color)";
+		button.style.background = "#DDD0C8";
+		button.style.color = "#323232";
+		button.style.border = "solid var(--secondary-color)";
 		links.forEach((link) => {
 			link.style.color = "#323232";
 		});
