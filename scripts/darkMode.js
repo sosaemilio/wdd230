b01a525c514cc3bdeeefb0097c78b1bf280e3dce
Emilio Sosa <emiliossosa85@gmail.com>
2023-01-29 20:38:03 -0400

    Fix test

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -4,7 +4,8 @@ const links = document.querySelectorAll("a");
 const navBar = document.querySelector("nav");
 const footer = document.querySelector("footer");
 const menu = document.querySelector("#menu-icon");
-const templeLabel = document.querySelector("#temple-label")
+const templeLabel = document.querySelector("#temple-label");
+const submitBtn = document.querySelector("#btnSubmit");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.src.includes("dark-mode-icon.svg")) {
@@ -17,18 +18,28 @@ modeButton.addEventListener("click", () => {
 		footer.style.background = "#121212";
 		templeLabel.style.background = "#121212";
 		templeLabel.style.color = "#fff";
+		submitBtn.style.background = "#323232";
+		submitBtn.style.color  = "#fff";
+		submitBtn.style.border = "#323232";
+
+		/* -- Menu Icon -- */
 		modeButton.src = "images/light-mode-icon.svg";
 		modeButton.classList.toggle("white-icon");
 		menu.classList.toggle("white-icon");
 	} else {
-		modeButton.src = "images/dark-mode-icon.svg";
-		main.style.background = "#fff"
-		main.style.color = "#323232"
+		main.style.background = "#fff";
+		main.style.color = "#323232";
 		navBar.style.background = "#DDD0C8";
 		footer.style.background = "#DDD0C8";
+		submitBtn.style.background = "#DDD0C8";
+		submitBtn.style.color = "#323232";
+		submitBtn.style.border = "solid var(--secondary-color)";
 		links.forEach((link) => {
 			link.style.color = "#323232";
 		});
+
+		/* -- Menu Icon -- */
+		modeButton.src = "images/dark-mode-icon.svg";
 		modeButton.classList.toggle("black-icon");
 		menu.classList.toggle("black-icon");
 	}
