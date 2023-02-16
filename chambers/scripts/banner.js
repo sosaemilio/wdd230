f63886f115ca1ab2cc97bf2b999e615f2783f4a4
Emilio Sosa <emiliossosa85@gmail.com>
2023-02-16 01:38:47 -0400

    Fix announcement banner

--- chambers/scripts/banner.js
+++ chambers/scripts/banner.js
@@ -3,11 +3,12 @@ const announcement = document.createElement("section");
 announcement.classList.toggle("announcement-bar");
 
 function setBanner (today) {
-    if(today.getDay() == 1 || today.getDay() == 2 || today.getDay() == 4 ) {
+    if(today.getDay() == 1 || today.getDay() == 2 || today.getDay() == 3 ) {
         //document.querySelector("#hero-banner").style.background = `url("https://sosaemilio.github.io/wdd230/chambers/images/meet-greet-wednesday.webp")`;
         
         let content = document.createElement("p");
-        content.textContent = "You are invited to join our Meet and Greet on Wednesday at 7:00 PM on the Maremares Hotel";
+        content.innerHTML = "<strong>You are invited to join our Meet and Greet on Wednesday at 7:00 PM. Location: Maremares Hotel (Venetur)</strong>";
+        content.style.margin = "0";
 
         announcement.appendChild(content);
         document.querySelector(".hero-section").after(announcement);
